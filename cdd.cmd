8f0f20886fa1fdd0b228af1d14c9c0c228f20d47
Pogromist <12221569+PogromistDev@users.noreply.github.com>
2019-09-28 23:23:59 +0400

    Upload
    Uploading my cool batch スクリプト 🥴

---
+++ cdd.cmd
@@ -0,0 +1,4 @@
+@echo off
+rem changes current directory to user's Desktop folder
+
+cd %USERPROFILE%\Desktop
---
+++ ip.cmd
@@ -0,0 +1,4 @@
+@echo off
+rem shows external ip address
+
+nslookup myip.opendns.com. resolver1.opendns.com
